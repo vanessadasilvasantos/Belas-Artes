e2fc2895e4fc16ff0c350c579760b9e8a5bb11a5
vanessadasilvasantos <vanessadasilvasantos65@gmail.com>
2025-03-07 13:35:04 -0300

    configuracoes de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
-import {BrowserRouter} from 'react-router'
+import {HashRouter } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter >
     <App />
-  </BrowserRouter>
+  </HashRouter >
 )
